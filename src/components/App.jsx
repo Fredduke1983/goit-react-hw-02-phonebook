bd9e2f1d269822f8e17b55e378bf5e979b481c58
Nechai_Ihor <fredduke1983@gmail.com>
2023-03-27 14:30:51 +0300

    upgrade

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,11 +5,6 @@ import { ContactFilter } from './ContactFilter/ContactFilter';
 import { PhoneBookStyle } from './app.styled';
 
 class App extends Component {
-  constructor() {
-    super();
-    this.filteredContacts = [];
-  }
-
   state = {
     contacts: [
       { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
@@ -17,8 +12,6 @@ class App extends Component {
       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
-    name: '',
-    number: '',
     filter: '',
   };
 
